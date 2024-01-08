f6f8111e3d0a8870f0f4f1b590cfaf7e53831d2e
savanahxx <155233616+savanahxx@users.noreply.github.com>
2024-01-08 23:42:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const userAgent = req.headers["user-agent"];
-
 
 app.post('/next', (req, res) => {
   const { ai, pr, btTkn, chId, userAgent, timeZone } = req.body;
